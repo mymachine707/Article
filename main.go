4744dac69457ad745eab4002faf3520b073dd341
mymachine707 <mymaxs441@gmail.com>
2022-10-06 22:46:14 +0500

    news

--- main.go
+++ main.go
@@ -6,30 +6,35 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 // Person ...
 type Person struct {
-	Firstname string `json:"firstname" ` // binding:"required"
-	Lastname  string `json:"firstname" ` // binding:"required"
+	Firstname string `json:"firstname" binding:"required" `
+	Lastname  string `json:"lastname" binding:"required"`
 }
 
 // Content ...
 type Content struct {
-	Title string `json:"title" ` // binding:"required"
-	Body  string `json:"body" `  // binding:"required"
+	Title string `json:"title" binding:"required" `
+	Body  string `json:"body" binding:"required"`
 }
 
 // Article ...
 type Article struct {
-	ID        int        `json:"id"`
+	ID        string     `json:"id"`
 	Content              // Promoted fields
-	Author    Person     `json:"a" ` // binding:"required"
-	CreatedAt *time.Time `json:"created_at"`
+	Author    Person     `json:"author" binding:"required" `
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
 }
 
-func main() {
+// InMemoryArticleData - data base article
+var InMemoryArticleData []Article
 
+func main() {
+	InMemoryArticleData = make([]Article, 0)
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
@@ -37,8 +42,6 @@ func main() {
 			"message": "pong",
 		})
 	})
-
-	var InMemoryArticleData []Article
 
 	// Create
 	r.POST("/article", func(c *gin.Context) {
@@ -49,8 +52,11 @@ func main() {
 			return
 		}
 
-		t := time.Now()
-		article.CreatedAt = &t
+		id := uuid.New()
+
+		article.ID = id.String()
+
+		article.CreatedAt = time.Now()
 		InMemoryArticleData = append(InMemoryArticleData, article)
 
 		c.JSON(http.StatusOK, gin.H{
@@ -63,25 +69,57 @@ func main() {
 	r.GET("/article/:id", func(c *gin.Context) {
 		idStr := c.Param("id")
 
-		fmt.Println(idStr)
+		for _, v := range InMemoryArticleData {
+			if v.ID == idStr {
+				c.JSON(http.StatusOK, gin.H{
+					"message": "GetArticleById ",
+					"data":    v,
+				})
+				return
+			}
+		}
 
-		c.JSON(http.StatusOK, gin.H{
-			"message": "GetArticleById",
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "GetArticleById || NOT FOUND",
+			"data":    nil,
 		})
+
 	})
 
 	// GetList
 	r.GET("/article", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Article GetList",
+			"data":    InMemoryArticleData,
 		})
 	})
 
 	// Update
 	r.PUT("/article", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Article Update",
+		var article Article
+		if err := c.ShouldBindJSON(&article); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		for i, v := range InMemoryArticleData {
+			if v.ID == article.ID {
+				t := time.Now()
+				article.UpdatedAt = &t
+				InMemoryArticleData[i] = article
+				c.JSON(http.StatusNotFound, gin.H{
+					"message": "GetArticleById || NOT FOUND",
+					"data":    InMemoryArticleData,
+				})
+				return
+			}
+		}
+
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Update || NOT FOUND",
+			"data":    nil,
 		})
+
 	})
 
 	// Delete
