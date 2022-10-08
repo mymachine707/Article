d6f4f3e98727f1f119b843106b6f552983a94531
mymachine707 <mymaxs441@gmail.com>
2022-10-08 10:54:16 +0500

    grupirovka va postman ishladi

---
+++ handlar.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func remove(slice []Article, s int) []Article {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+// InMemoryArticleData - data base article
+var InMemoryArticleData []Article
+
+//CreatArticle ...
+func CreatArticle(c *gin.Context) {
+
+	var article Article
+	if err := c.ShouldBindJSON(&article); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	id := uuid.New()
+	article.ID = id.String()
+
+	article.CreatedAt = time.Now()
+	InMemoryArticleData = append(InMemoryArticleData, article)
+
+	c.JSON(http.StatusOK, gin.H{
+		"data":    InMemoryArticleData,
+		"message": "Article Create ",
+	})
+}
+
+// GetArticleByID ...
+func GetArticleByID(c *gin.Context) {
+	idStr := c.Param("id")
+
+	for _, v := range InMemoryArticleData {
+		if v.ID == idStr {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "GetArticleById",
+				"data":    v,
+			})
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{
+		"message": "GetArticleById || NOT FOUND",
+		"data":    nil,
+	})
+
+}
+
+// GetArticleList ...
+func GetArticleList(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Article GetList",
+		"data":    InMemoryArticleData,
+	})
+}
+
+// ArticleUpdate ...
+func ArticleUpdate(c *gin.Context) {
+	var article Article
+	if err := c.ShouldBindJSON(&article); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	for i, v := range InMemoryArticleData {
+		if v.ID == article.ID {
+			article.CreatedAt = v.CreatedAt
+			t := time.Now()
+			article.UpdatedAt = &t
+			InMemoryArticleData[i] = article
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Article Update",
+				"data":    InMemoryArticleData,
+			})
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{
+		"message": "Update || NOT FOUND",
+		"data":    nil,
+	})
+
+}
+
+// DeleteArticle ...
+func DeleteArticle(c *gin.Context) {
+	idStr := c.Param("id")
+
+	for i, v := range InMemoryArticleData {
+		if v.ID == idStr {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Article Deleted",
+				"data":    v,
+			})
+			InMemoryArticleData = remove(InMemoryArticleData, i)
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{
+		"message": "Article || Delete || NOT FOUND",
+		"data":    nil,
+	})
+}
--- main.go
+++ main.go
@@ -2,35 +2,9 @@ package main
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 )
-
-// Person ...
-type Person struct {
-	Firstname string `json:"firstname" binding:"required" `
-	Lastname  string `json:"lastname" binding:"required"`
-}
-
-// Content ...
-type Content struct {
-	Title string `json:"title" binding:"required" `
-	Body  string `json:"body" binding:"required"`
-}
-
-// Article ...
-type Article struct {
-	ID        string     `json:"id"`
-	Content              // Promoted fields
-	Author    Person     `json:"author" binding:"required" `
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt *time.Time `json:"updated_at"`
-}
-
-// InMemoryArticleData - data base article
-var InMemoryArticleData []Article
 
 func main() {
 	InMemoryArticleData = make([]Article, 0)
@@ -42,124 +16,16 @@ func main() {
 		})
 	})
 
-	// Create
-	r.POST("/article", CreatArticle)
+	// Gruppirovka qilindi
+	v1 := r.Group("v1")
+	{
 
-	// GetArticleById
-	r.GET("/article/:id", GetArticleByID)
-
-	// GetList
-	r.GET("/article", GetArticleList)
-
-	// Update
-	r.PUT("/article", ArticleUpdate)
-
-	// Delete
-	r.DELETE("/article/:id", DeleteArticle)
+		v1.POST("/article", CreatArticle)
+		v1.GET("/article/:id", GetArticleByID)
+		v1.GET("/article", GetArticleList)
+		v1.PUT("/article", ArticleUpdate)
+		v1.DELETE("/article/:id", DeleteArticle)
+	}
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-}
-
-func remove(slice []Article, s int) []Article {
-	return append(slice[:s], slice[s+1:]...)
-}
-
-//CreatArticle ...
-func CreatArticle(c *gin.Context) {
-
-	var article Article
-	if err := c.ShouldBindJSON(&article); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	id := uuid.New()
-	article.ID = id.String()
-
-	article.CreatedAt = time.Now()
-	InMemoryArticleData = append(InMemoryArticleData, article)
-
-	c.JSON(http.StatusOK, gin.H{
-		"data":    InMemoryArticleData,
-		"message": "Article Create ",
-	})
-}
-
-// GetArticleByID ...
-func GetArticleByID(c *gin.Context) {
-	idStr := c.Param("id")
-
-	for _, v := range InMemoryArticleData {
-		if v.ID == idStr {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "GetArticleById ",
-				"data":    v,
-			})
-			return
-		}
-	}
-
-	c.JSON(http.StatusNotFound, gin.H{
-		"message": "GetArticleById || NOT FOUND",
-		"data":    nil,
-	})
-
-}
-
-// GetArticleList ...
-func GetArticleList(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Article GetList",
-		"data":    InMemoryArticleData,
-	})
-}
-
-// ArticleUpdate ...
-func ArticleUpdate(c *gin.Context) {
-	var article Article
-	if err := c.ShouldBindJSON(&article); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	for i, v := range InMemoryArticleData {
-		if v.ID == article.ID {
-			article.CreatedAt = v.CreatedAt
-			t := time.Now()
-			article.UpdatedAt = &t
-			InMemoryArticleData[i] = article
-			c.JSON(http.StatusOK, gin.H{
-				"message": "Article Update",
-				"data":    InMemoryArticleData,
-			})
-			return
-		}
-	}
-
-	c.JSON(http.StatusNotFound, gin.H{
-		"message": "Update || NOT FOUND",
-		"data":    nil,
-	})
-
-}
-
-// DeleteArticle ...
-func DeleteArticle(c *gin.Context) {
-	idStr := c.Param("id")
-
-	for i, v := range InMemoryArticleData {
-		if v.ID == idStr {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "Article Deleted",
-				"data":    v,
-			})
-			InMemoryArticleData = remove(InMemoryArticleData, i)
-			return
-		}
-	}
-
-	c.JSON(http.StatusNotFound, gin.H{
-		"message": "Article || Delete || NOT FOUND",
-		"data":    nil,
-	})
 }
