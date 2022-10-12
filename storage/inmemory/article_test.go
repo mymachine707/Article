48f5b9fc138ab01d7256906896237a415384a5fa
mymachine707 <mymaxs441@gmail.com>
2022-10-12 16:38:38 +0500

    s

--- storage/inmemory/article_test.go
+++ storage/inmemory/article_test.go
@@ -1,7 +1,45 @@
 package inmemory_test
 
-import "testing"
+import (
+	"errors"
+	"mymachine707/models"
+	"mymachine707/storage/inmemory"
+	"testing"
+)
 
 func TestAddArticle(t *testing.T) {
-	t.Log("Salom Islombek")
+
+	var err error
+	IM := inmemory.InMemory{
+		Db: &inmemory.DB{},
+	}
+
+	if err != nil {
+		t.Fatalf("unexpextedError: %v", err)
+	}
+
+	expextedError := errors.New("author not found")
+
+	if err != nil && err.Error() != expextedError.Error() {
+		t.Errorf("IM.AddArticle() expexted: %v, but got error: %v", expextedError, err)
+	}
+
+	err = IM.AddAuthor("b3546729-0695-4c63-ba3d-c3caa7310cde", models.CreateAuthorModul{
+		Firstname: "John",
+		Lastname:  "Doe",
+	})
+
+	err = IM.AddArticle("836e2951-8190-40b3-8d02-3a6a6b34f4a5", models.CreateArticleModul{
+		Content: models.Content{
+			Title: "Lorem",
+			Body:  "Impsum",
+		},
+		AuthorID: "b3546729-0695-4c63-ba3d-c3caa7310cde",
+	})
+
+	if err != nil && err.Error() != expextedError.Error() {
+		t.Errorf("IM.AddArticle() expexted: %v, but got error: %v", expextedError, err)
+	}
+
+	t.Log("Test has been finished!")
 }
